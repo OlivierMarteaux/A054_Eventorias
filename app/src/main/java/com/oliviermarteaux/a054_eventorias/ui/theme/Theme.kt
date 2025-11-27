78ab99209f38ce279f99ba0067fe69bd4453c686
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-11-27 14:24:19 +0100

    splash screen: layout finalized

--- app/src/main/java/com/oliviermarteaux/a054_eventorias/ui/theme/Theme.kt
+++ app/src/main/java/com/oliviermarteaux/a054_eventorias/ui/theme/Theme.kt
@@ -8,12 +8,13 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -36,9 +37,10 @@ private val LightColorScheme = lightColorScheme(
 fun A054_EventoriasTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false/*true*/,
     content: @Composable () -> Unit
 ) {
+    val darkTheme = true
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current

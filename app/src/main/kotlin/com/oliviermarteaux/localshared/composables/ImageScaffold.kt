9b9fae9169ce74dfe4afd1ddfdbba5fc43eeeefd
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-11-29 16:37:05 +0100

    login screen: layout finalized

---
+++ app/src/main/kotlin/com/oliviermarteaux/localshared/composables/ImageScaffold.kt
@@ -0,0 +1,86 @@
+package com.oliviermarteaux.localshared.composables
+
+import android.R.attr.top
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.oliviermarteaux.shared.composables.SharedImage
+
+/**
+ * A scaffold that displays an icon and some content.
+ *
+ * @param modifier The modifier to apply to this scaffold.
+ * @param content The content to display.
+ */
+@Composable
+fun ImageScaffold(
+    image: Painter,
+    modifier: Modifier = Modifier,
+    imageModifier: Modifier = Modifier,
+    horizontalPadding: Dp = 0.dp,
+    formPortraitHorizontalPadding: Dp = 0.dp,
+    innerPadding: PaddingValues = PaddingValues(),
+    content: @Composable () -> Unit
+) {
+    val configuration = LocalConfiguration.current
+    val orientation = configuration.orientation
+
+    if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.padding(innerPadding)
+        ) {
+            SharedImage(
+                painter = image,
+                modifier = imageModifier
+                    .weight(1f)
+                    .padding(horizontal = horizontalPadding)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(2f)
+                    .padding(horizontal = formPortraitHorizontalPadding)
+            ) {
+                content()
+            }
+        }
+    } else {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .fillMaxHeight()
+                .padding(horizontal = horizontalPadding)
+                .consumeWindowInsets(innerPadding),
+        ) {
+            SharedImage(
+                painter = image,
+                modifier = imageModifier.weight(1f)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(2f)
+                    .padding(start =  horizontalPadding, top =  32.dp, bottom =  24.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                content()
+            }
+        }
+    }
+}
--- app/src/main/kotlin/com/oliviermarteaux/localshared/firebase/authentication/ui/screen/splash/SplashScreen.kt
+++ app/src/main/kotlin/com/oliviermarteaux/localshared/firebase/authentication/ui/screen/splash/SplashScreen.kt
@@ -1,16 +1,16 @@
 package com.oliviermarteaux.localshared.firebase.authentication.ui.screen.splash
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults.contentPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -18,9 +18,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.oliviermarteaux.a054_eventorias.R
+import com.oliviermarteaux.localshared.composables.ImageScaffold
 import com.oliviermarteaux.localshared.composables.SharedButton
 import com.oliviermarteaux.shared.composables.IconSource
-import com.oliviermarteaux.shared.composables.SharedImage
 import com.oliviermarteaux.shared.composables.SharedScaffold
 
 /**
@@ -41,46 +41,76 @@ fun SplashScreen(
         SharedScaffold(
             modifier = modifier,
         ) { innerPadding ->
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize()
-                    .padding(innerPadding)
-                    .padding(horizontal = 85.dp),
+            ImageScaffold(
+                image = painterResource(id = logoDrawableRes),
+                innerPadding = innerPadding,
+                horizontalPadding = 85.dp,
+                formPortraitHorizontalPadding = 85.dp,
+                imageModifier = Modifier.height( 120.dp),
             ) {
-                SharedImage(
-                    painter = painterResource(id = logoDrawableRes),
-                    modifier = Modifier
-                        .height( 120.dp)
-                        .weight(1f)
+                SharedButton(
+                    onClick = { signInWithGoogle(navigateToHomeScreen) },
+                    text = stringResource(R.string.sign_in_with_Google),
+                    textColor = Color.Black,
+                    icon = IconSource.PainterIcon(painterResource(id = R.drawable.ic_google_logo)),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                    shape = MaterialTheme.shapes.extraSmall,
+                    contentPadding = PaddingValues(16.dp),
+                    modifier = Modifier.fillMaxWidth(),
                 )
                 Spacer(Modifier.height(24.dp))
-                Column(
-                    modifier = Modifier.weight(2f)
-                ) {
-                    SharedButton(
-                        onClick = { signInWithGoogle(navigateToHomeScreen) },
-                        text = stringResource(R.string.sign_in_with_Google),
-                        textColor = Color.Black,
-                        icon = IconSource.PainterIcon(painterResource(id = R.drawable.ic_google_logo)),
-                        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
-                        shape = MaterialTheme.shapes.extraSmall,
-                        contentPadding = PaddingValues(16.dp),
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                    Spacer(Modifier.height(24.dp))
 
-                    SharedButton(
-                        onClick = navigateToLoginScreen,
-                        text = stringResource(R.string.sign_in_with_email),
-                        textColor = Color.White,
-                        icon = IconSource.PainterIcon(painterResource(id = R.drawable.ic_mail_white_no_outline)),
-                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD32F2F)),
-                        shape = MaterialTheme.shapes.extraSmall,
-                        contentPadding = PaddingValues(16.dp),
-                        modifier = Modifier.fillMaxWidth(),
-                    )
-                }
+                SharedButton(
+                    onClick = navigateToLoginScreen,
+                    text = stringResource(R.string.sign_in_with_email),
+                    textColor = Color.White,
+                    icon = IconSource.PainterIcon(painterResource(id = R.drawable.ic_mail_white_no_outline)),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD32F2F)),
+                    shape = MaterialTheme.shapes.extraSmall,
+                    contentPadding = PaddingValues(16.dp),
+                    modifier = Modifier.fillMaxWidth(),
+                )
             }
+//            Column(
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                modifier = Modifier.fillMaxSize()
+//                    .padding(innerPadding)
+//                    .padding(horizontal = 85.dp),
+//            ) {
+//                SharedImage(
+//                    painter = painterResource(id = logoDrawableRes),
+//                    modifier = Modifier
+//                        .height( 120.dp)
+//                        .weight(1f)
+//                )
+//                Spacer(Modifier.height(24.dp))
+//                Column(
+//                    modifier = Modifier.weight(2f)
+//                ) {
+//                    SharedButton(
+//                        onClick = { signInWithGoogle(navigateToHomeScreen) },
+//                        text = stringResource(R.string.sign_in_with_Google),
+//                        textColor = Color.Black,
+//                        icon = IconSource.PainterIcon(painterResource(id = R.drawable.ic_google_logo)),
+//                        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+//                        shape = MaterialTheme.shapes.extraSmall,
+//                        contentPadding = PaddingValues(16.dp),
+//                        modifier = Modifier.fillMaxWidth(),
+//                    )
+//                    Spacer(Modifier.height(24.dp))
+//
+//                    SharedButton(
+//                        onClick = navigateToLoginScreen,
+//                        text = stringResource(R.string.sign_in_with_email),
+//                        textColor = Color.White,
+//                        icon = IconSource.PainterIcon(painterResource(id = R.drawable.ic_mail_white_no_outline)),
+//                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD32F2F)),
+//                        shape = MaterialTheme.shapes.extraSmall,
+//                        contentPadding = PaddingValues(16.dp),
+//                        modifier = Modifier.fillMaxWidth(),
+//                    )
+//                }
+//            }
         }
     }
 }

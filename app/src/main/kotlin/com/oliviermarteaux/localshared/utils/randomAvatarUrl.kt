a165e33c5434fc78583240db482ef69cdee4f98e
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-12-27 15:24:09 +0100

    migrated permission and notification (commented)

--- app/src/main/kotlin/com/oliviermarteaux/localshared/utils/randomAvatarUrl.kt
+++
@@ -1,9 +0,0 @@
-package com.oliviermarteaux.localshared.utils
-
-//fun randomAvatarUrl(): String {
-//    val genders = listOf("men", "women")
-//    val gender = genders.random()
-//    val id = (0..99).random()  // RandomUser has portraits from 0 to 99
-//
-//    return "https://randomuser.me/api/portraits/$gender/$id.jpg"
-//}
--- app/src/main/kotlin/com/oliviermarteaux/localshared/utils/randomPhoto.kt
+++
@@ -1,3 +0,0 @@
-package com.oliviermarteaux.localshared.utils
-
-//fun randomPhoto(): String = "https://picsum.photos/800/600?random=1"

2e86c955609140ea5b8c442ac2d3ed52fce70368
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-01-01 15:06:55 +0100

    update sonar.yml

--- app/src/androidTest/java/com/oliviermarteaux/a054_eventorias/cucumber/steps/LoginSteps.kt
+++ app/src/androidTest/java/com/oliviermarteaux/a054_eventorias/cucumber/steps/LoginSteps.kt
@@ -32,7 +32,7 @@ class LoginSteps(private val composeRuleHolder: ComposeRuleHolder) {
     @Given("I am on the Home screen")
     fun iAmOnTheHomeScreen() {
         // Verify some known events are shown
-        composeRule.waitUntil(5000) {
+        composeRule.waitUntil(60000) {
             composeRule.onNodeWithText("City Carnival").isDisplayed()
 //            composeRule.onNodeWithText("Science Festival").isDisplayed()
         }

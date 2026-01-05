76731885b07c7323ebd7bdb68ad133e71eaa9e8e
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-01-05 11:13:03 +0100

    test sonar conf with only profile feature test

--- app/src/main/java/com/oliviermarteaux/a054_eventorias/ui/screen/account/AccountViewModel.kt
+++ app/src/main/java/com/oliviermarteaux/a054_eventorias/ui/screen/account/AccountViewModel.kt
@@ -100,8 +100,21 @@ class AccountViewModel @Inject constructor(
     }
 
     init {
-        userUiState = UiState.Loading
-        getCurrentUser()
-        getNotifState()
+        if (BuildConfig.DEBUG) {
+            val fakeUser = User(
+                id = "123",
+                firstname = "Fievel",
+                lastname = "Farwest",
+                fullname = "Fievel Farwest",
+                email = "fievelfarwest@example.com",
+            )
+            userUiState = UiState.Success(fakeUser)
+            user = fakeUser
+            notificationState = true
+        } else {
+            userUiState = UiState.Loading
+            getCurrentUser()
+            getNotifState()
+        }
     }
 }

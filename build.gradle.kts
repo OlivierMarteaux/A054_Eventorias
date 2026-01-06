cf13a8aea2f3bb804de72c604b4a735d178394e2
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-01-06 14:11:29 +0100

    test clean build

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     // for Jetpack Compose
     alias(libs.plugins.kotlin.compose) apply false // Compose Compiler Gradle plugin required from Kotlin 2.0
     // for Hilt DI
-//    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     // Add the dependency for the Google services Gradle plugin for Firebase authentication
     alias(libs.plugins.googleservices) apply false

b85476857a83b3656a6dc4df9e7ba88a1eb43c48
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-01-06 22:00:24 +0100

    cucumber test features: home - detail - profile

--- app/src/main/java/com/oliviermarteaux/a054_eventorias/ui/screen/account/AccountViewModel.kt
+++ app/src/main/java/com/oliviermarteaux/a054_eventorias/ui/screen/account/AccountViewModel.kt
@@ -1,24 +1,19 @@
 package com.oliviermarteaux.a054_eventorias.ui.screen.account
 
-import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
-import com.oliviermarteaux.a054_eventorias.EventoriasApplication
+import com.oliviermarteaux.shared.datastore.NotificationPreferencesRepository
 import com.oliviermarteaux.shared.firebase.authentication.data.repository.UserRepository
-import com.oliviermarteaux.shared.firebase.authentication.domain.mapper.toUser
 import com.oliviermarteaux.shared.firebase.authentication.domain.model.User
 import com.oliviermarteaux.shared.firebase.authentication.ui.screen.AuthUserViewModel
-import com.oliviermarteaux.shared.datastore.NotificationPreferencesRepository
 import com.oliviermarteaux.shared.ui.UiState
 import com.oliviermarteaux.shared.utils.CoroutineDispatcherProvider
 import com.oliviermarteaux.shared.utils.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import okhttp3.internal.platform.PlatformRegistry.applicationContext
 import javax.inject.Inject
 
 @HiltViewModel
